da9549d514e6ca01d932666b9ed2d1f362b15ef2
bzasc <brunolnascimento@gmail.com>
2025-06-18 16:51:31 -0300

    nvim: add typescript LSP to mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,19 +2,20 @@
 -- * Manager for external tools (LSPs, linters, debuggers, formatters)
 -- * auto-install those external tools
 return {
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	dependencies = {
-		{ "williamboman/mason.nvim", opts = true },
-		{ "williamboman/mason-lspconfig.nvim", opts = true },
-	},
-	opts = {
-		ensure_installed = {
-			"pyright", -- LSP for python
-			"ruff", -- linter & formatter (includes flake8, pep8, black, isort, etc.)
-			"debugpy", -- debugger
-			"taplo", -- LSP for toml (e.g., for pyproject.toml files)
-			"prettier", -- opinionated code formatter,
-			"stylua", -- A lua code formatter
-		},
-	},
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = {
+    { "williamboman/mason.nvim", opts = true },
+    { "williamboman/mason-lspconfig.nvim", opts = true },
+  },
+  opts = {
+    ensure_installed = {
+      "pyright", -- LSP for python
+      "ruff", -- linter & formatter (includes flake8, pep8, black, isort, etc.)
+      "debugpy", -- debugger
+      "taplo", -- LSP for toml (e.g., for pyproject.toml files)
+      "prettier", -- opinionated code formatter,
+      "stylua", -- A lua code formatter
+      "typescript-language-server", -- LSP for typescript
+    },
+  },
 }
